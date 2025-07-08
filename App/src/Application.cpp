3d1f7a9fe43f5aaa3c5b5e47bf2d5d97bab80265
Adamska-01 <silverworld@hotmail.it>
2025-07-08 17:29:50 +0200

    Removed all example content

--- App/src/Application.cpp
+++ App/src/Application.cpp
@@ -1,11 +1,8 @@
 #include "Application.h"
-#include "Scenes/ExampleScene.h"
 #include <SubSystems/Window.h>
 
 
 Application::Application()
 {
-	Window::SetWindowTitle("Hello World!");
-
-	SceneManager::LoadScene<ExampleScene>();
+	Window::SetWindowTitle("Retro Platformer");
 }
